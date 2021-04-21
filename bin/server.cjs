64105e70ea825fc6e4a048772f32faedaf747766
Dmytro Shalenyi <shalenyi.dm@gmail.com>
2021-04-21 23:07:51 +0300

    Update server.cjs

--- bin/server.cjs
+++ bin/server.cjs
@@ -3,11 +3,14 @@ const app = require('../app.cjs')
 
 const PORT = process.env.PORT || 3000
 
-db.then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server running. Use our API on port: ${PORT}`)
-  })
-}).catch(err => {
-  console.log(`Server not running. Error message: ${err.message}`)
-  process.exit(1)
-})
+;(async db => {
+  try {
+    await db
+    app.listen(PORT, () => {
+      console.log(`Server running. Use our API on port: ${PORT}`)
+    })
+  } catch (err) {
+    console.log(`Server not running. Error message: ${err.message}`)
+    process.exit(1)
+  }
+})(db)
